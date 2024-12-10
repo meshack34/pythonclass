1844184858c6f8c73cab10806682df107d5aa166
meshack34 <meshackkimutai34@gmail.com>
2024-12-10 23:06:44 +0300

    variable

---
+++ variable2.py
@@ -0,0 +1,17 @@
+num_1 = 8 ; num_2 = 10 ; num_3 = 11.8
+
+print(num_1); print(num_2)
+
+print(num_2 + num_1)
+
+print("hello world")
+print('hello world')
+
+fruits = ('apple', 'banana', 'orange')
+
+
+person = {'name': 'Alice', 'age': 30, 'city': 'NewYork'}
+print(person)
+
+
+print(fruits)
---
+++ varibles.py
@@ -0,0 +1,9 @@
+print ("hello world")
+print ("My first python program !")
+
+d5 = 10 
+
+if True:
+    print("yes it is true")
+    print("d5")
+    
