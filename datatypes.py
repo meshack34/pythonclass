bd5078d9a6fb6a0aa344138db2821764efcec25d
meshack34 <meshackkimutai34@gmail.com>
2024-12-11 23:43:51 +0300

    all

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+x = 10.0
+y = 8 + 8j
+
+print(type(y))
+print(type(x))
+
+st1 = "hello "
+
+print(st1[5])
---
+++ list.py
@@ -0,0 +1,28 @@
+#list
+
+list1 =[]
+
+list2 = [ 'car']
+
+list3 = ['TOYOTA','SEDANS','FORD','TESLA']
+
+list4 = [['goats','cows' ,'sheep'],['fruits' , 'vegetables'], ['cars','buses','lorry']]
+        
+print(list3[3])                                                      
+print(list4[-1][0])
+
+#tuple
+tuple1 = tuple([1,2,3,5]) ; 
+print(tuple1[1])
+
+# bolean
+   
+print(type (True))
+print(type (False))
+
+#set
+
+set1 = set()
+set2 = set([1,"gee", 3,5,10, "new"])
+
+print (set2)
