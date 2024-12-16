fb177899fb00fdaffc41f57cd3358af89b197d19
meshack34 <meshackkimutai34@gmail.com>
2024-12-16 15:48:09 +0300

    expessions

---
+++ arithmetics.py
@@ -0,0 +1,7 @@
+m= int(input(" Enter first Number  :")) 
+x= int(input(" Enter secound Number  :"))
+
+sum =x+m
+sub =x-m
+
+print(sum, sub)
---
+++ expession.py
@@ -0,0 +1,18 @@
+y= 10 ; 
+x=5 + y 
+print(x)
+
+name = "jane"
+greetings =  "hello " + name 
+print(greetings)
+
+x=11
+is_equal = x==19
+print(is_equal)
+
+width =20
+height = 10
+
+area= width * height
+
+print(area)
